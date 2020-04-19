8eaf90590060dc0265f25d1cc40a24dbd583fb35
pavanirayidi <62829691+pavanirayidi@users.noreply.github.com>
2020-04-19 02:26:42 -0400

    Python Fundamental Probelm12

---
+++ generate sentence.py
@@ -0,0 +1,14 @@
+
+def generate_sentences(subjects,verbs,objects):
+    #start writing your code here
+    sentence_list=[]
+    for i in subjects:
+        for j in verbs:
+            for k in objects:
+                sentence_list.append(i+" "+j+" "+k)
+    return sentence_list
+
+subjects=["I","You"]
+verbs=["love", "play"]
+objects=["Hockey","Football"]
+print(generate_sentences(subjects,verbs,objects))
