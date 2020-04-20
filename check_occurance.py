17a2ad3a3770caaf0f5d4f4a20578092ac482848
pavanirayidi <62829691+pavanirayidi@users.noreply.github.com>
2020-04-20 00:20:00 -0400

    python fundamental probelm 26

---
+++ check_occurance.py
@@ -0,0 +1,15 @@
+'''Given a string, write a python function to return True if the strings "mat" and "jet" appear the same number of times in the given string, else return False.
+Note: Perform case insensitive string comparison wherever necessary.
+
+Sample Input	Expected Output
+Jet on the Mat but mat is too long :	False
+mat jet Jet Mat	:True'''
+
+
+#PF-Prac-26
+def check_occurence(string):
+    #start writing your code here
+    return string.count("Jet")==string.count("Mat")
+        
+string="string-Mat is beautiful compared to the jet Get a new Jet"
+print(check_occurence(string))
